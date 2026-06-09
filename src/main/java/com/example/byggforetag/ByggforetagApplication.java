8a65f227bca2f36d601e26285266e87c343e9476
arvid <arvideriksson28@gmail.com>
2026-06-09 19:52:10 +0200

    initial commit

---
+++ src/main/java/com/example/byggforetag/ByggforetagApplication.java
@@ -0,0 +1,13 @@
+package com.example.byggforetag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByggforetagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByggforetagApplication.class, args);
+	}
+
+}
