16a74fd700a2f9c08775ff5e347802d8f7b4daf6
arvid <arvideriksson28@gmail.com>
2026-08-12 23:36:42 +0200

    add endpoints for jobassignment

--- src/main/java/com/example/byggforetag/Controller/AdminController.java
+++ src/main/java/com/example/byggforetag/Controller/AdminController.java
@@ -1,10 +1,12 @@
 package com.example.byggforetag.Controller;
 
 import com.example.byggforetag.DTO.EmployeeDto;
+import com.example.byggforetag.DTO.JobAssignmentDto;
 import com.example.byggforetag.DTO.JobDto;
 import com.example.byggforetag.DTO.UserDto;
 import com.example.byggforetag.Enums.JobStatus;
 import com.example.byggforetag.Service.EmployeeService;
+import com.example.byggforetag.Service.JobAssignmentService;
 import com.example.byggforetag.Service.JobService;
 import com.example.byggforetag.Service.UserService;
 import org.springframework.http.HttpStatus;
@@ -19,11 +21,13 @@ public class AdminController {
     private final UserService userService;
     private final JobService jobService;
     private final EmployeeService employeeService;
+    private final JobAssignmentService jobAssignmentService;
 
-    public AdminController(UserService userService, JobService jobService, EmployeeService employeeService) {
+    public AdminController(UserService userService, JobService jobService, EmployeeService employeeService, JobAssignmentService jobAssignmentService) {
         this.userService = userService;
         this.jobService = jobService;
         this.employeeService = employeeService;
+        this.jobAssignmentService = jobAssignmentService;
     }
 
     @PostMapping("/register/employee")
@@ -76,5 +80,21 @@ public class AdminController {
     @PutMapping("/employees/{id}")
     public ResponseEntity<EmployeeDto> updateEmployee(@PathVariable Long id, @RequestBody EmployeeDto employeeDto){
         return ResponseEntity.ok(employeeService.updateEmployee(employeeDto, id));
+    }
+
+    @PostMapping("/jobassignments/assignEmployee")
+    public ResponseEntity<JobAssignmentDto> assignEmployee(@RequestBody JobAssignmentDto jobAssignmentDto){
+        return ResponseEntity.status(HttpStatus.CREATED).body(jobAssignmentService.assignEmployee(jobAssignmentDto));
+    }
+
+    @DeleteMapping("/jobassignments/{id}")
+    public ResponseEntity<Void> removeEmployee(@PathVariable Long id){
+        jobAssignmentService.removeEmployee(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/jobassignments/{id}")
+    public ResponseEntity<List<JobAssignmentDto>> getAllJobAssignments(@PathVariable Long id){
+        return ResponseEntity.ok(jobAssignmentService.getJobAssignmentByJobId(id));
     }
 }
