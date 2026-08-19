0ca06f042ada7d1022a111525f48cf1e3cc8cdba
arvid <arvideriksson28@gmail.com>
2026-08-19 11:27:23 +0200

    add ConversationNotFoundException, ConversationRepository and ConversationParticipantRepository

---
+++ src/main/java/com/example/byggforetag/Repository/ConversationParticipantRepository.java
@@ -0,0 +1,8 @@
+package com.example.byggforetag.Repository;
+
+import com.example.byggforetag.Model.ConversationParticipant;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConversationParticipantRepository extends JpaRepository<ConversationParticipant, Long> {
+
+}
--- src/main/java/com/example/byggforetag/Repository/ConversationRepository.java
+++ src/main/java/com/example/byggforetag/Repository/ConversationRepository.java
@@ -2,6 +2,14 @@ package com.example.byggforetag.Repository;
 
 import com.example.byggforetag.Model.Conversation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface ConversationRepository extends JpaRepository<Conversation, Long> {
+    Optional<Conversation> findConversationByJobId(Long jobId);
+    @Query("SELECT c FROM Conversation c JOIN c.conversationParticipants cp WHERE cp.user.id = :userId")
+    List<Conversation> findConversationByUserId(@Param("userId") Long userId);
 }
