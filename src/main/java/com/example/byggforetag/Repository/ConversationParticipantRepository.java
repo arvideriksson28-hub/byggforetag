6c4066b40bad2557168c726c4786e53539d404bf
arvid <arvideriksson28@gmail.com>
2026-08-19 13:53:16 +0200

    add @Repository to Repositorys

--- src/main/java/com/example/byggforetag/Repository/ConversationParticipantRepository.java
+++ src/main/java/com/example/byggforetag/Repository/ConversationParticipantRepository.java
@@ -2,7 +2,9 @@ package com.example.byggforetag.Repository;
 
 import com.example.byggforetag.Model.ConversationParticipant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ConversationParticipantRepository extends JpaRepository<ConversationParticipant, Long> {
 
 }
--- src/main/java/com/example/byggforetag/Repository/QuoteRepository.java
+++ src/main/java/com/example/byggforetag/Repository/QuoteRepository.java
@@ -2,9 +2,10 @@ package com.example.byggforetag.Repository;
 
 import com.example.byggforetag.Model.Quote;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface QuoteRepository extends JpaRepository<Quote, Long> {
 
     Optional<Quote> findQuoteByJobId(Long jobId);
