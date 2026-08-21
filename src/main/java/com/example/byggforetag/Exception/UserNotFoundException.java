3ab7d0039274d9854b1a8a09bc63cf90c0979874
arvid <arvideriksson28@gmail.com>
2026-08-21 12:55:39 +0200

    change from one dto to request and response for User

--- src/main/java/com/example/byggforetag/Exception/UserNotFoundException.java
+++ src/main/java/com/example/byggforetag/Exception/UserNotFoundException.java
@@ -3,6 +3,10 @@ package com.example.byggforetag.Exception;
 public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(Long id) {
 
-        super("Användaren hittades inte med id: " + id);
+        super("Användaren hittades inte med id: " + id );
+    }
+
+    public UserNotFoundException(String email){
+        super("Hittade ingen användare med email: " + email);
     }
 }
