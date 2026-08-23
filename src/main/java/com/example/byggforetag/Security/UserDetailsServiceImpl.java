2ccf2894f2131af05dbfc1df8227dc6a9d9081bc
arvid <arvideriksson28@gmail.com>
2026-08-23 22:29:19 +0200

    add AuthController, AuthRequestDto, JwtAuthFilter, JwtService, SecurityConfig and UserDetailsServiceImpl

---
+++ src/main/java/com/example/byggforetag/Security/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.example.byggforetag.Security;
+
+import com.example.byggforetag.Model.User;
+import com.example.byggforetag.Repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Användaren hittades inte med Email: " + email));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .roles(user.getRole().name())
+                .build();
+    }
+}
